6d7ec73eddccc78c9f4050a56b098cc30e201e7d
Elena <batichev22@gmail.com>
2021-08-23 14:43:08 +0300

    java 8

---
+++ src/main/java/homework/java8/ListUtils.java
@@ -0,0 +1,40 @@
+package homework.java8;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ListUtils {
+
+
+    public static <T> void forEachWithDelay(List<T> list, int delay, Consumer<T> consumer){
+        list.forEach(elementList -> {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            consumer.accept(elementList);
+        });
+    }
+
+
+    public static <T> int countDuplicate(T value, List<T> list) {
+        return Collections.frequency(list, value);
+    }
+
+    public static <T> int countDuplicate(T value, List<T> list, Equalator<T> equalator) {
+
+        int counter = 0;
+
+        for (T t : list) {
+            if (equalator.equals(t, value)) {
+                counter++;
+            }
+
+        }
+        return counter;
+
+
+    }
+}
